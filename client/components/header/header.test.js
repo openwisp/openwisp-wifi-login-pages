179acd931f58c694011bf7ced47225cb882b931d
Vivek Chand <V.rajput40@gmail.com>
2019-06-07 15:45:13 +0530

    [tests] Setup Jest with Enzyme for testing react components #5
    closes #5

---
+++ client/components/header/header.test.js
@@ -0,0 +1,11 @@
+import {mount} from "enzyme";
+import React from "react";
+
+import Header from "./header";
+
+describe("App Header", () => {
+	it("should render without crashing", () => {
+		const component = mount(<Header />);
+		expect(component).toMatchSnapshot();
+	});
+});
---
+++ client/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+
+import Header from "./components/header";
+
+class Routes extends React.Component {
+	render() {
+		return (
+			<Switch>
+				<Route path="/" render={props => <Header {...props} />} />
+			</Switch>
+		);
+	}
+}
+export default Routes;
