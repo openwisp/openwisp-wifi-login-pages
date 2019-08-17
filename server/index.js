c5baf48bdbbb0b2dd5b0e31698638a66a7e01453
Vivek Chand <V.rajput40@gmail.com>
2019-08-17 07:45:37 +0530

    [config] write config to serve prodution build from node server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
+import compression from "compression";
 import cookieParser from "cookie-parser";
 import express from "express";
+import cookiesMiddleware from "universal-cookie-express";
 
 import routes from "./routes";
 
-const app = express();
-const cookiesMiddleware = require("universal-cookie-express");
+const path = require("path");
 
+const app = express();
+app.use(compression());
+app.use(express.static(path.join(process.cwd(), "dist")));
 app.use(cookieParser());
 app.use(cookiesMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/api/v1/:organization/account", routes.account);
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(process.cwd(), "dist", "index.html"));
+});
 app.listen(3030, () => {
   console.log("Server started on port 3030");
 });
