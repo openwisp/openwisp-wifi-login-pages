e8eb4585bf5e0fa5cd07cb1b26ea56a24c82861c
Vivek Chand <V.rajput40@gmail.com>
2020-01-25 10:56:33 +0530

    Update password change component

--- client/components/password-change/password-change.js
+++ client/components/password-change/password-change.js
@@ -1,12 +1,13 @@
 import "./index.css";
-import React from "react";
+
 import axios from "axios";
-import qs from "qs";
 import PropTypes from "prop-types";
+import qs from "qs";
+import React from "react";
 
-import {passwordConfirmError, passwordChangeApiUrl} from "../../constants";
-
+import { passwordChangeApiUrl, passwordConfirmError } from "../../constants";
 import getText from "../../utils/get-text";
+import history from "../../utils/history";
 
 export default class PasswordChange extends React.Component {
   constructor(props) {
@@ -22,10 +23,9 @@ export default class PasswordChange extends React.Component {
 
   handleSubmit(e) {
     if (e) e.preventDefault();
-    const {orgSlug} = this.props;
+    const { orgSlug } = this.props;
     const url = passwordChangeApiUrl.replace("{orgSlug}", orgSlug);
-    const {newPassword1, newPassword2} = this.state;
-
+    const { newPassword1, newPassword2 } = this.state;
     if (newPassword1 !== newPassword2) {
       this.setState({
         errors: {
@@ -45,60 +45,132 @@ export default class PasswordChange extends React.Component {
         newPassword1,
         newPassword2,
       }),
-    }).catch(error => console.log(error));
+    }).then(() => {
+      history.replace(`/${orgSlug}/status`);
+    }).catch(() => {
+      this.setState({
+        errors: {
+          nonField: `Some error occured. Couldn't change password!`,
+        },
+      });
+    });
   }
 
   handleChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
-    const {language, passwordChange} = this.props;
-    const {errors} = this.state;
+    const { language, passwordChange } = this.props;
+    const { errors, newPassword1, newPassword2 } = this.state;
     return (
-      <div className="owisp-passchange-page">
-        <div className="owisp-passchange-container">
-          <div className="title">{getText(passwordChange.title, language)}</div>
-          <div className="owisp-passchange-label">
-            {getText(passwordChange.input_fields.password1.label, language)}
-          </div>
+      <div className="owisp-password-change-container">
+        <form className="owisp-password-change-form" onSubmit={this.handleSubmit}>
+          <div className="owisp-password-change-form-title">{getText(passwordChange.title, language)}</div>
+          {passwordChange.input_fields.password1 ?
+            <>
+              <label className="owisp-password-change-label owisp-password-change-label-password"
+                htmlFor='owisp-password-change-password'
+              >
+                <div className='owisp-password-change-label-text'>
+                  {getText(passwordChange.input_fields.password1.label, language)}
+                </div>
+                <input
+                  className="owisp-password-change-input owisp-password-change-input-password"
+                  type="password"
+                  id='owisp-password-change-password'
+                  name="newPassword1"
+                  required
+                  value={newPassword1}
+                  placeholder={getText(
+                    passwordChange.input_fields.password1.placeholder,
+                    language,
+                  )}
+                  pattern={
+                    passwordChange.input_fields.password1.pattern
+                      ? passwordChange.input_fields.password1.pattern
+                      : undefined
+                  }
+                  onChange={e => this.handleChange(e)}
+                />
+              </label>
+              {errors.newPassword1 ?
+                <div className="owisp-password-change-error">
+                  <span className='owisp-password-change-error-icon'>!</span>
+                  <span className='owisp-password-change-error-text'>
+                    {errors.newPassword1}
+                  </span>
+                </div>
+                : null}
+            </> : null}
+          {passwordChange.input_fields.password2 ?
+            <>
+              <label className="owisp-password-change-label owisp-password-change-label-password-confirm"
+                htmlFor='owisp-password-change-password-confirm'
+              >
+                <div className='owisp-password-change-label-text'>
+                  {getText(passwordChange.input_fields.password1.label, language)}
+                </div>
+                <input
+                  className="owisp-password-change-input owisp-password-change-input-password-confirm"
+                  type="password"
+                  name="newPassword2"
+                  id='owisp-password-change-password-confirm'
+                  required
+                  value={newPassword2}
+                  placeholder={getText(
+                    passwordChange.input_fields.password2.placeholder,
+                    language,
+                  )}
+                  title={
+                    passwordChange.input_fields.password1.pattern_description
+                      ? getText(
+                        passwordChange.input_fields.password1
+                          .pattern_description,
+                        language,
+                      )
+                      : undefined
+                  }
+                  onChange={e => this.handleChange(e)}
+                />
+              </label>
+              {errors.newPassword2 ?
+                <div className="owisp-password-change-error">
+                  <span className='owisp-password-change-error-icon'>!</span>
+                  <span className='owisp-password-change-error-text'>
+                    {errors.newPassword2}
+                  </span>
+                </div>
+                : null}
+            </> : null}
+          {errors.nonField ?
+            <div className="owisp-password-change-error">
+              <span className='owisp-password-change-error-icon'>!</span>
+              <span className='owisp-password-change-error-text'>
+                {errors.nonField}
+              </span>
+            </div>
+            : null}
           <input
-            className="owisp-passchange-input"
-            style={{marginTop: "1rem"}}
-            type="password"
-            name="newPassword1"
-            placeholder={getText(
-              passwordChange.input_fields.password1.placeholder,
-              language,
-            )}
-            onChange={e => this.handleChange(e)}
+            type='submit'
+            className="owisp-password-change-btn owisp-password-change-btn-submit"
+            value={getText(passwordChange.buttons.submit_button.text, language)}
+            pattern={
+              passwordChange.input_fields.password2.pattern
+                ? passwordChange.input_fields.password2.pattern
+                : undefined
+            }
+            title={
+              passwordChange.input_fields.password2.pattern_description
+                ? getText(
+                  passwordChange.input_fields.password2
+                    .pattern_description,
+                  language,
+                )
+                : undefined
+            }
           />
-          <div className="owisp-passchange-label">
-            {getText(passwordChange.input_fields.password1.label, language)}
-          </div>
-          <input
-            className="owisp-passchange-input"
-            type="password"
-            name="newPassword2"
-            placeholder={getText(
-              passwordChange.input_fields.password2.placeholder,
-              language,
-            )}
-            onChange={e => this.handleChange(e)}
-          />
-          <div className="owisp-passchange-error">
-            {errors.newPassword2 ? errors.newPassword2 : null}
-          </div>
-          <div
-            className="owisp-passchange-submit"
-            tabIndex="0"
-            role="button"
-            onClick={() => this.handleSubmit()}
-            onKeyDown={() => this.handleSubmit()}
-          >
-            {getText(passwordChange.buttons.submit_button.text, language)}
-          </div>
-        </div>
+        </form>
       </div>
     );
   }
--- client/components/status/status.js
+++ client/components/status/status.js
@@ -4,15 +4,16 @@ import axios from "axios";
 import PropTypes from "prop-types";
 import qs from "qs";
 import React from "react";
-import {Cookies} from "react-cookie";
+import { Cookies } from "react-cookie";
+import { Link } from 'react-router-dom';
 
-import {validateApiUrl} from "../../constants";
+import { validateApiUrl } from "../../constants";
 import getText from "../../utils/get-text";
 import Contact from "../contact-box";
 
 export default class Status extends React.Component {
   componentDidMount() {
-    const {cookies, orgSlug, logout} = this.props;
+    const { cookies, orgSlug, logout } = this.props;
     const token = cookies.get(`${orgSlug}_auth_token`);
     const url = validateApiUrl.replace("{orgSlug}", orgSlug);
     return axios({
@@ -36,8 +37,8 @@ export default class Status extends React.Component {
   }
 
   render() {
-    const {statusPage, language, orgSlug, logout, cookies} = this.props;
-    const {content, links, buttons} = statusPage;
+    const { statusPage, language, orgSlug, logout, cookies } = this.props;
+    const { content, links, buttons } = statusPage;
     const contentArr = getText(content, language).split("\n");
     return (
       <React.Fragment>
@@ -69,14 +70,14 @@ export default class Status extends React.Component {
                   ) : null}
                   {links
                     ? links.map(link => (
-                        <a
-                          className="owisp-status-link"
-                          key={link.url}
-                          href={link.url.replace("{orgSlug}", orgSlug)}
-                        >
-                          {getText(link.text, language)}
-                        </a>
-                      ))
+                      <Link
+                        className="owisp-status-link"
+                        key={link.url}
+                        to={link.url.replace("{orgSlug}", orgSlug)}
+                      >
+                        {getText(link.text, language)}
+                      </Link>
+                    ))
                     : null}
                   <input
                     type="button"
