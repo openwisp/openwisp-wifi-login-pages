87bcd7e2395618a213a0ef0ab3f555cf55d6fe16
s0mnaths <somnaths027@gmail.com>
2021-03-21 21:05:19 +0530

    [footer]Fix unique key props error #169
    Made the changes requested and changed the key to {index}
    
    Fixes #169

--- client/components/footer/footer.js
+++ client/components/footer/footer.js
@@ -24,7 +24,7 @@ export default class Footer extends React.Component {
                   footer-link-${index + 1}`}
                     target="_blank"
                     rel="noreferrer noopener"
-                    key={link}
+                    key={index}
                   >
                     {getText(link.text, language)}
                   </a>
