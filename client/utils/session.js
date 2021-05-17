c04c7fba8002eda940fc79531e56a01d44150aff
sankalp <sankalp123427@gmail.com>
2021-05-17 20:09:50 +0530

    [chore] Add comments in handleSession utils function

--- client/utils/session.js
+++ client/utils/session.js
@@ -1,4 +1,11 @@
 const handleSession = (orgSlug, token, cookies) => {
+  /*
+  This function is created while implementation of remember me checkbox
+  It is used to check whether sessionStorage or cookies is used to store
+  the token. If session storage is used then it will clear the cookies
+  and returns the token with session - true else it returns the token
+  with session - false.
+  */
   const sessionKey = sessionStorage.getItem(`${orgSlug}_auth_token`);
   if (sessionKey) {
     cookies.remove(`${orgSlug}_auth_token`, {path: "/"});
