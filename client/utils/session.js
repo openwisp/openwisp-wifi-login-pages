7c37725cf93fd38709ce3b6d4264ee5a21d4df7c
sankalp <sankalp123427@gmail.com>
2021-05-19 22:05:20 +0530

    [requested-change] Made block comment better to understand

--- client/utils/session.js
+++ client/utils/session.js
@@ -1,11 +1,13 @@
 const handleSession = (orgSlug, token, cookies) => {
-  /*
-  This function is created while implementation of remember me checkbox
-  It is used to check whether sessionStorage or cookies is used to store
-  the token. If session storage is used then it will clear the cookies
-  and returns the token with session - true else it returns the token
-  with session - false.
-  */
+  /**
+   * This function is used to check whether sessionStorage or cookies
+   * is used to store the token. If remember-me checkbox is unchecked
+   * then sessionStorage will be used to store the token and all the
+   * cookies will be cleared. It will return an object which contains
+   * the token and session with value equals true.
+   * If remember-me checkbox is checked then it will not clear any cookies
+   * and returns an object with token and session with value equals false.
+   */
   const sessionKey = sessionStorage.getItem(`${orgSlug}_auth_token`);
   if (sessionKey) {
     cookies.remove(`${orgSlug}_auth_token`, {path: "/"});
