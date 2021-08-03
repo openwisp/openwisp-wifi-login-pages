a5cff33036542235646419279a59e8c7dea9e821
sankalp <sankalp123427@gmail.com>
2021-08-03 12:56:01 +0530

    [tests] Complete coverage of modal component

--- client/components/modal/index.js
+++ client/components/modal/index.js
@@ -2,7 +2,7 @@ import {connect} from "react-redux";
 
 import Component from "./modal";
 
-const mapStateToProps = (state, ownProps) => {
+export const mapStateToProps = (state, ownProps) => {
   return {
     privacyPolicy: state.organization.configuration.privacy_policy,
     termsAndConditions: state.organization.configuration.terms_and_conditions,
