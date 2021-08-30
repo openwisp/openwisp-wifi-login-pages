8b387dfb2bf541298d29a0a5ddbe7cc6052e1009
Sankalp <sankalp123427@gmail.com>
2021-08-31 00:11:30 +0530

    [feature] Added browser based tests using selenium #237
    * [change] Added test for password change and reset
    * [tests] Added browser based test for mobile verification flow
    * [tests] Added browser based tests for mobile phone change
    * [change] Added wait for initializing data and clear data
    * [change] Create org config script for browser tests
    * [test] Added test for language
    * [change] Renamed file and formatted code
    * [change] Simplified create mobile configuration script
    
    Closes #237

--- browser-test/initialize_data.py
+++ browser-test/initialize_data.py
@@ -22,6 +22,7 @@ if OPENWISP_RADIUS_PATH == '':
 
 # do not initialize data for registration tests
 registration_tests = 'register' in sys.argv
+create_mobile_verification_org = 'mobileVerification' in sys.argv
 
 sys.path.insert(0, os.path.join(OPENWISP_RADIUS_PATH, 'tests'))
 sys.argv.insert(1, 'browser-test')
@@ -41,6 +42,8 @@ from swapper import load_model
 User = get_user_model()
 Organization = load_model('openwisp_users', 'Organization')
 OrganizationUser = load_model('openwisp_users', 'OrganizationUser')
+OrganizationRadiusSettings = load_model('openwisp_radius', 'OrganizationRadiusSettings')
+RegisteredUser = load_model('openwisp_radius', 'RegisteredUser')
 
 test_data = load_test_data()
 test_user_email = test_data['testuser']['email']
@@ -50,6 +53,25 @@ test_user_organization = test_data['testuser']['organization']
 if registration_tests:
     User.objects.filter(username=test_user_email).delete()
     sys.exit(0)
+
+if create_mobile_verification_org:
+    data = test_data['mobileVerificationTestUser']
+    org = Organization.objects.create(name=data['organization'])
+    settings = OrganizationRadiusSettings.objects.create(
+        organization=org,
+        needs_identity_verification=True,
+        sms_verification=True,
+        sms_sender=data['email'],
+    )
+    user = User.objects.create_user(
+        username=data['phoneNumber'],
+        password=data['password'],
+        email=data['email'],
+        phone_number=data['phoneNumber'],
+    )
+    RegisteredUser.objects.create(user=user, method=data['method'])
+    OrganizationUser.objects.create(organization=org, user=user)
+
 
 try:
     org = Organization.objects.get(slug=test_user_organization)
---
+++ browser-test/mobile-verfication.test.js
@@ -0,0 +1,91 @@
+import {until, By} from "selenium-webdriver";
+import {
+  getDriver,
+  getElementByCss,
+  urls,
+  initialData,
+  initializeData,
+  clearData,
+  getPhoneToken,
+} from "./utils";
+
+const fillPhoneField = async (driver, data) => {
+  async function fillField() {
+    const username = await driver.wait(
+      until.elementLocated(By.css("input#username")),
+    );
+    await driver.wait(until.elementIsVisible(username));
+    await username.sendKeys(data.phoneNumber);
+  }
+  try {
+    await fillField();
+  } catch (err) {
+    // Stale Reference Error due to DOM reload by PhoneInput Field
+    await new Promise((r) => setTimeout(r, 1000));
+    await fillField();
+  }
+};
+
+describe("Selenium tests for <MobileVerification />", () => {
+  let driver;
+
+  beforeAll(async () => {
+    await initializeData("mobileVerification");
+    driver = await getDriver();
+  }, 30000);
+
+  afterAll(async () => {
+    await clearData();
+    await driver.manage().deleteAllCookies();
+    driver.close();
+  });
+
+  it("should test mobile verification flow (with one failed attempt and successful attempt)", async () => {
+    const data = initialData().mobileVerificationTestUser;
+    await driver.get(urls.verificationLogin(data.organization));
+    const password = await getElementByCss(driver, "input#password");
+    password.sendKeys(data.password);
+    await fillPhoneField(driver, data);
+    let submitBtn = await getElementByCss(driver, "input[type=submit]");
+    submitBtn.click();
+    const successToastDiv = await getElementByCss(driver, "div[role=alert]");
+    await driver.wait(until.elementIsVisible(successToastDiv));
+    expect(await driver.getCurrentUrl()).toEqual(
+      urls.mobileVerification(data.organization),
+    );
+    expect(await successToastDiv.getText()).toEqual("Login successful");
+    driver.navigate().refresh();
+    const codeInput = await getElementByCss(driver, "input#code");
+    await driver.wait(until.elementIsVisible(codeInput));
+    codeInput.sendKeys("123456");
+    submitBtn = await getElementByCss(driver, "button[type='submit']");
+    await driver.wait(until.elementIsVisible(submitBtn));
+    submitBtn.click();
+    const failureToastDiv = await getElementByCss(driver, "div[role=alert]");
+    await driver.wait(until.elementIsVisible(failureToastDiv));
+    expect(await failureToastDiv.getText()).toEqual("Invalid code.");
+    const token = getPhoneToken();
+    codeInput.clear();
+    codeInput.sendKeys(token);
+    submitBtn = await getElementByCss(driver, "button[type='submit']");
+    await driver.wait(until.elementIsVisible(submitBtn));
+    submitBtn.click();
+    await getElementByCss(driver, "div#status");
+    const emailElement = await getElementByCss(
+      driver,
+      "div > p:nth-child(5) > span",
+    );
+    expect(await emailElement.getText()).toEqual(data.email);
+    const phoneElement = await getElementByCss(
+      driver,
+      "div > p:nth-child(6) > span",
+    );
+    expect(await phoneElement.getText()).toEqual(data.phoneNumber);
+    await driver.wait(until.urlContains("status"), 5000);
+    const activeSessionTr = await getElementByCss(
+      driver,
+      "table tr.active-session",
+    );
+    await driver.wait(until.elementIsVisible(activeSessionTr));
+  });
+});
