2cf319e2b8087513d529c4f03ab7ed88bb99d1a6
Federico Capoano <f.capoano@openwisp.io>
2021-08-31 11:21:15 -0500

    [fix] Fix handleLogout in payment status
    Fixed payment status cancel button, the handleLogout
    function now uses history.push to avoid the full page reload.

--- client/utils/handle-logout.js
+++ client/utils/handle-logout.js
@@ -1,10 +1,11 @@
 import {toast} from "react-toastify";
 import {t} from "ttag";
 import {initialState} from "../reducers/organization";
+import history from "./history";
 
 const redirectToStatus = (setUserData, userData, orgSlug) => {
   setUserData({...userData, mustLogout: true});
-  window.location.assign(`${window.location.origin}/${orgSlug}/status`);
+  history.push(`/${orgSlug}/status`);
 };
 
 const handleLogout = (logout, cookies, orgSlug, setUserData, userData) => {
