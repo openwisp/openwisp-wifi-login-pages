a722558883db2837e363a423e427fc1ad961a12e
Federico Capoano <f.capoano@openwisp.io>
2021-09-03 12:20:16 -0500

    [change] Changed log directory from logs/ to log/
    For consistency with other OpenWISP modules.

--- server/loggerConfig.js
+++ server/loggerConfig.js
@@ -1,10 +1,10 @@
 const logFilePath = {
-  all: process.env.ALL_LOG_FILE || "logs/all.log",
-  error: process.env.ERROR_LOG_FILE || "logs/error.log",
-  warn: process.env.WARN_LOG_FILE || "logs/warn.log",
-  info: process.env.INFO_LOG_FILE || "logs/info.log",
-  http: process.env.HTTP_LOG_FILE || "logs/http.log",
-  debug: process.env.DEBUG_LOG_FILE || "logs/debug.log",
+  all: process.env.ALL_LOG_FILE || "log/all.log",
+  error: process.env.ERROR_LOG_FILE || "log/error.log",
+  warn: process.env.WARN_LOG_FILE || "log/warn.log",
+  info: process.env.INFO_LOG_FILE || "log/info.log",
+  http: process.env.HTTP_LOG_FILE || "log/http.log",
+  debug: process.env.DEBUG_LOG_FILE || "log/debug.log",
 };
 
 export default logFilePath;
