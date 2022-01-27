a2241e37c0dcb192506b5ed455707f9d6d3a3562
sankalp <sankalp123427@gmail.com>
2022-01-27 23:35:38 +0530

    [change] Use secure cookies when in production #515
    Closes #515

--- server/utils/send-cookies.js
+++ server/utils/send-cookies.js
@@ -10,15 +10,16 @@ const sendCookies = (response, conf, res, user = null) => {
   const authTokenCookie = cookie.sign(response.data.key, conf.secret_key);
   const usernameCookie = cookie.sign(username, conf.secret_key);
   // forward response
+  const options = {
+    maxAge: 1000 * 60 * 60 * 24,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  };
   return res
     .status(response.status)
     .type("application/json")
-    .cookie(`${conf.slug}_auth_token`, authTokenCookie, {
-      maxAge: 1000 * 60 * 60 * 24,
-    })
-    .cookie(`${conf.slug}_username`, usernameCookie, {
-      maxAge: 1000 * 60 * 60 * 24,
-    })
+    .cookie(`${conf.slug}_auth_token`, authTokenCookie, options)
+    .cookie(`${conf.slug}_username`, usernameCookie, options)
     .send(response.data);
 };
 export default sendCookies;
