a28fbef0f232ac7584840370899d19e0fa0117e8
enock295simiyu <enocksimiyu8@gmail.com>
2024-09-04 16:46:49 +0300

    Restart app after change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "wifi_login_pages",
     script: "/opt/openwisp/wifi-login-pages/server/start.js", // the path of the script you want to execute,
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    instances: 1,
+    instances: 2,
     autorestart: true,
     watch: ["dist", "organizations", "server"],
     // Specify delay between watch interval
