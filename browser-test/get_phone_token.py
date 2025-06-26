2b8212bb0d476e3491545634c018a40867fbe3d6
Federico Capoano <f.capoano@openwisp.io>
2025-06-26 18:41:43 -0300

    [qa] Switched python code to double quotes

--- browser-test/get_phone_token.py
+++ browser-test/get_phone_token.py
@@ -8,26 +8,26 @@ import django
 
 def load_test_data():
     project_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    browser_test_path = os.path.join(project_path, 'browser-test')
-    test_data_file = open(os.path.join(browser_test_path, 'testData.json'))
+    browser_test_path = os.path.join(project_path, "browser-test")
+    test_data_file = open(os.path.join(browser_test_path, "testData.json"))
     test_data = json.loads(test_data_file.read())
     test_data_file.close()
     return test_data
 
 
-OPENWISP_RADIUS_PATH = os.environ.get('OPENWISP_RADIUS_PATH', '')
-if OPENWISP_RADIUS_PATH == '':
-    print('OPENWISP_RADIUS_PATH is needed.', file=sys.stderr)
+OPENWISP_RADIUS_PATH = os.environ.get("OPENWISP_RADIUS_PATH", "")
+if OPENWISP_RADIUS_PATH == "":
+    print("OPENWISP_RADIUS_PATH is needed.", file=sys.stderr)
     sys.exit(1)
 
-sys.path.insert(0, os.path.join(OPENWISP_RADIUS_PATH, 'tests'))
-sys.argv.insert(1, 'browser-test')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openwisp2.settings')
+sys.path.insert(0, os.path.join(OPENWISP_RADIUS_PATH, "tests"))
+sys.argv.insert(1, "browser-test")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openwisp2.settings")
 try:
     django.setup()
 except ImportError:
     print(
-        'OpenWISP RADIUS is not installed or python virtual environment is not activated correctly',
+        "OpenWISP RADIUS is not installed or python virtual environment is not activated correctly",
         file=sys.stderr,
     )
     sys.exit(1)
@@ -36,16 +36,16 @@ from django.contrib.auth import get_user_model
 from swapper import load_model
 
 User = get_user_model()
-PhoneToken = load_model('openwisp_radius', 'PhoneToken')
+PhoneToken = load_model("openwisp_radius", "PhoneToken")
 
 test_data = load_test_data()
 try:
     user = User.objects.filter(
-        username=test_data['mobileVerificationTestUser']['phoneNumber']
+        username=test_data["mobileVerificationTestUser"]["phoneNumber"]
     ).first()
     phone_token = PhoneToken.objects.filter(user=user).first()
     sys.stdout.write(phone_token.token)
     sys.exit(0)
 except Exception as e:
-    sys.stderr.write(f'{e}')
+    sys.stderr.write(f"{e}")
     sys.exit(1)
