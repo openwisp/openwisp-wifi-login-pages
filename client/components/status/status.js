abfcaa0be745e5905fe23c0b5c396955c865104c
Pranshu <pranshusumanstuff@gmail.com>
2025-09-26 04:08:32 +0530

    [fix] Use calling_station_id instead of macaddr for MAC filtering #914
    Status component used param `macaddr` to filter
    users by MAC address but Radius session uses the
    param `calling_station_id`. Caused it to return all
    sessions instead of filtering properly.
    
    Fixes #914

--- client/components/status/status.js
+++ client/components/status/status.js
@@ -216,7 +216,7 @@ export default class Status extends React.Component {
 
       const macaddr = cookies.get(`${orgSlug}_macaddr`);
       if (macaddr) {
-        const params = {macaddr};
+        const params = {calling_station_id: macaddr};
         await this.getUserActiveRadiusSessions(params);
         if (statusPage.radius_usage_enabled) {
           await this.getUserRadiusUsage();
