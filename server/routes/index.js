cbc857b2a91c3a751b6771adc29032685147a3a6
Federico Capoano <f.capoano@openwisp.io>
2025-12-23 11:54:11 +0100

    [change] Removed Captive Portal API (unused)

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,10 @@ import account from "./account";
 import plans from "./plans";
 import modal from "./modal";
 import payment from "./payment";
-import captivePortal from "./captive-portal";
 
 export default {
   account,
   plans,
   modal,
   payment,
-  captivePortal,
 };
