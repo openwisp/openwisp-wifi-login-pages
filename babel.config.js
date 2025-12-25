05373392ecc6943ef6324cec3085058f6f3c804b
Ajay110125yadav <ajayyadavpersonal52@gmail.com>
2025-12-25 23:35:50 +0530

    Fix formatting and dependency issues found by QA checks

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ["@babel/plugin-proposal-class-properties", {loose: true}],
     ["@babel/plugin-transform-arrow-functions"],
     ["@babel/plugin-syntax-dynamic-import"],
-    ["@babel/plugin-proposal-private-methods", {loose: true}],
+    ["@babel/plugin-transform-private-methods", {loose: true}],
     ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
     ["@babel/plugin-transform-spread"],
     "transform-remove-strict-mode",
