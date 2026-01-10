b61ba6b67b6278daff7c584386dea9ae60965b4e
Iishaan Tanwar <93773731+PRoIISHAAN@users.noreply.github.com>
2026-01-10 21:16:35 +0530

    [fix] Removed unused dependencies, added knip to CI QA checks #1007
    - Added a QA check which complains if unused dependencies, exports, imports, files are present
    - Removed unused dependencies
    - Added excplicit nodeJS setup in github actions workflows
    
    Closes #1007
    
    ---------
    
    Co-authored-by: Federico Capoano <f.capoano@openwisp.io>

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,11 @@ module.exports = {
     ["@babel/plugin-proposal-class-properties", {loose: true}],
     ["@babel/plugin-transform-arrow-functions"],
     ["@babel/plugin-syntax-dynamic-import"],
+<<<<<<< HEAD
     ["@babel/plugin-transform-private-methods", {loose: true}],
+=======
+    ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
+>>>>>>> 1eb25ed ([fix] Removed unused dependencies, added knip to CI QA checks #1007)
     ["@babel/plugin-transform-spread"],
     "transform-remove-strict-mode",
   ],
