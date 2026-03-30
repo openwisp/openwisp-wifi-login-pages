c9d3852bbc7e682e06783e2ac36994fcea76138a
https://github.com/soja902/Abdullahi-idris <programmingwithcoys@gmail.com>
2026-03-30 22:16:00 +0100

    Fail fast on translation build errors and fix formatting

--- config/build-test-translations.js
+++ config/build-test-translations.js
@@ -1,7 +1,6 @@
+/* eslint-disable no-console */
 const fs = require("fs");
-
 const path = require("path");
-
 const childProcess = require("child_process");
 
 const rootDir = process.cwd();
@@ -9,46 +8,44 @@ const i18nDir = path.join(rootDir, "i18n");
 const outputFile = path.join(rootDir, "client", "test-translation.json");
 
 function poToObject(file) {
+  const ttagBinary = path.join(
+    rootDir,
+    "node_modules",
+    ".bin",
+    process.platform === "win32" ? "ttag.cmd" : "ttag",
+  );
 
   try {
-    const result = childProcess.execSync(`npx ttag po2json "${path.join(i18nDir, file)}"`
-);
-  
+    const result = childProcess.execSync(
+      `"${ttagBinary}" po2json "${path.join(i18nDir, file)}"`,
+    );
     return JSON.parse(result.toString("utf-8"));
   } catch (err) {
     console.error(`Error while converting ${file}:`, err.message);
-    return {};
+    process.exit(1);
   }
 }
 
 function isBasePoFile(file) {
-
   return path.extname(file) === ".po" && !file.includes(".custom.po");
 }
 
 function buildTestTranslations() {
-
   const allFiles = fs.readdirSync(i18nDir);
   const poFiles = allFiles.filter(isBasePoFile);
 
   const enFile = poFiles.find((file) => path.basename(file, ".po") === "en");
   const selectedFile = enFile || poFiles[0];
 
-
   if (!selectedFile) {
     console.error("Error: no .po files found in i18n directory");
     process.exit(1);
-
   }
-
 
   const output = poToObject(selectedFile);
 
-  fs.writeFileSync(outputFile, JSON.stringify(output, null, 2));
+  fs.writeFileSync(outputFile, `${JSON.stringify(output, null, 2)}\n`);
   console.log(`Generated ${outputFile}`);
-
 }
-
-
 
 buildTestTranslations();
